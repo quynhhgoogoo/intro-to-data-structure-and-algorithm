d730a16bb454a4a4156a544fee3ec4277c83031e
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2020-09-30 15:13:38 +0300

    binary search for array

--- stack/stack-array.c
+++ stack/stack-array.c
@@ -55,6 +55,41 @@ int pop(struct Stack *st){
     return x;
 }
 
+/*Input : index of search element
+  Function: Searching for elements's value inside stack*/
+
+int peek(struct Stack st, int index){
+    int x = -1;
+    if(st.top-index+1 < 0){
+        printf("Invalid index\n");
+    }
+    x = st.S[st.top-index+1];
+    return x;
+}
+
+/*check if stack is empty*/
+int isEmpty(struct Stack *st){
+    if(st->top == -1){
+        return 1;
+    }
+    return 0;
+}
+
+/*check if stack is full*/
+int isFull(struct Stack st){
+    if(st.top == st.size - 1){
+        return 1;
+    }
+    return 0;
+}
+
+/*Return topmost element value inside stack*/
+int stackTop(struct Stack st){
+    if(!isEmpty(&st))
+        return st.S[st.top];
+    return -1;
+}
+
 int main(){
     struct Stack st;
     
@@ -62,6 +97,8 @@ int main(){
     push(&st, 10);
     push(&st, 20);
     push(&st, 30);
+
+    printf("%d \n", peek(st,2));
     Display(st);
     
     return 0;
