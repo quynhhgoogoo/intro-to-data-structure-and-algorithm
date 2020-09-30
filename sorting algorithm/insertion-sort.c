ce1cb1e417390901e0d4023c0144938e3720dd79
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2020-09-30 18:51:04 +0300

    insertion sort

---
+++ sorting algorithm/insertion-sort.c
@@ -0,0 +1,40 @@
+/*Compare consecutive pair of elements everytime*/
+#include <stdio.h>
+#include<stdlib.h>
+
+void swap(int *x, int *y){
+    int temp;
+
+    temp = *x ;
+    *x = *y;
+    *y = temp;
+}
+
+/*Input: Array and number of elements
+  Output: Void insertion sort*/
+void InsertionSort(int A[], int n){
+    int i, j, x;
+    
+    for(i=1; i<n; i++){
+        j = i-1;
+        x = A[i];
+
+        /*Shifting elements*/
+        while(j>-1 && A[j]>x){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = x;
+    }
+}
+
+int main(){
+    int A[] = {3,7,9,1,0,6,6,5,11,12}, n =10;
+    int i;
+
+
+    for (i=0; i<n; i++){
+        printf("%d ", A[i]);
+    }
+    return 0;
+}
