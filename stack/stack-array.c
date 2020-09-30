d55474a75598a17a40c789076f74d24f0156fa66
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2020-09-30 10:34:18 +0300

    stack

--- stack/stack-array.c
+++ stack/stack-array.c
@@ -1,51 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Stack{
+struct Stack
+{
     int size;
-    int top;    /*top pointer*/
-    int *S;     /*pointer to a dynamically created array*/
+    int top;        /*points to the latest element*/
+    int *S;         /*points to the array*/
 };
 
 /*Initialize a stack*/
-void create(struct Stack *st)
-{
+void create(struct Stack *st){
+    
     printf("Enter Size");
     scanf("%d",&st->size);
+
     st->top=-1;
     st->S=(int *)malloc(st->size*sizeof(int));
 }
 
-/*Display a stack*/
+/*Display elements inside the stack*/
 void Display(struct Stack st){
     int i;
-    for(i = st.top; i >= 0; i--){
-        printf("%d ", st.S[i]);
+
+    for(i=st.top;i>=0;i--){
+        printf("%d ",st.S[i]);
     }
     printf("\n");
 }
 
-/*Insert element to a stack*/
-void push(struct Stack *st, int value){
-    if(st->top == st->size-1){
-        printf("Stack is overflow/full");
+/*Insert new node inside stack*/
+void push(struct Stack *st,int x){
+
+    if(st->top==st->size-1){
+        printf("Stack overflow\n");
     }
+    
     else{
         st->top++;
-        st->S[st->top] = value;
-    }
+        st->S[st->top]=x;
+    } 
 }
 
-/*Delete an element in stack and return that element's value*/
+/*Delete/Pop the latest element inside stack*/
 int pop(struct Stack *st){
-    int x = -1;
-
-    if(st->top == -1){
-        printf("Stack is underflow/empty");
+    int x=-1;
+    
+    if(st->top==-1){
+        printf("Stack Underflow\n");
     }
     else{
+        x=st->S[st->top];
         st->top--;
-        x = st->S[st->top];
     }
     return x;
 }
