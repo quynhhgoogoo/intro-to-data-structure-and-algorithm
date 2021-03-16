ccaabdbdb7b671474255d792904efac3195fd7c4
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-03-16 22:19:57 +0200

    iterative tranversing

--- tree/iterative-order-using-stack.cpp
+++ tree/iterative-order-using-stack.cpp
@@ -198,6 +198,65 @@ void Tree::Postorder(Node *p) {
     }
 }
 
+/*Tranversing through tree itteratively*/
+void Tree::iterativePreorder(Node *p){
+    stack <Node*> stk;
+    while (p != nullptr || !stk.empty()){
+        if (!stk.empty()){
+            p = stk.top();
+            stk.pop();
+            p = p->rchild;
+        }
+        else{
+            cout << p->data << "," << flush;
+            stk.emplace(p);
+            p = p->lchild;
+        }
+    }
+    cout << endl;
+}
+
+void Tree::iterativeInorder(Node *p){
+    stack<Node *>stk;
+    while(p != nullptr || !stk.empty()){
+        if(p!=nullptr){
+            stk.emplace(p);
+            p = p->lchild;
+        }
+        else{
+            p = stk.top();
+            stk.pop();
+            cout << p->data << ", " << flush;
+            p = p->rchild;
+        }
+    }
+    cout<<endl;
+}
+
+void Tree::iterativePostorder(Node *p){
+    stack<long int>stk;
+    long int temp;
+
+    while(p != nullptr || !stk.empty()){
+        if(p!=nullptr){
+            stk.emplace((long int)p);
+            p = p->lchild;
+        }
+        else{
+            temp = stk.top();
+            stk.pop();
+            if (temp>0){
+                stk.emplace(-temp);
+                p = ((Node *)temp)->rchild;
+            }
+            else{
+                cout << ((Node *)(-1 * temp))->data << "," << flush;
+            }
+        }
+    }
+    cout<<endl;
+}
+
 int main(){
     Tree bt;
 
